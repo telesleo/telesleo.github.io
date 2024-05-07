1abd9651c5a879148b4f4fed61f87d41083bdd29
leo <leonardoandrei41@gmail.com>
2024-05-07 16:10:11 -0300

    Add margin to the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import Project from './pages/Project';
+import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/project/:projectId" element={<Project />} />
-    </Routes>
+    <div id="app">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/project/:projectId" element={<Project />} />
+      </Routes>
+    </div>
   );
 }
 
