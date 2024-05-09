b1ad593facf367b609f7b2baa34cac683df80384
leo <leonardoandrei41@gmail.com>
2024-05-09 15:03:45 -0300

    Add padding and border and remove text decoration from ProjectCard

--- src/pages/ProjectCard/index.jsx
+++ src/pages/ProjectCard/index.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './project-card.module.css';
 
-export default function ProjectCard({ title, description, image }) {
+export default function ProjectCard({
+  projectId, title, description, image,
+}) {
   return (
-    <div className={styles.card}>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <div
-        className={styles.image}
-        style={{
-          backgroundImage: `url(images/${image})`,
-        }}
-      />
-    </div>
+    <Link className={styles['project-link']} to={`/project/${projectId}`}>
+      <div className={styles.card}>
+        <h2>{title}</h2>
+        <p className={styles.description}>{description}</p>
+        <div
+          className={styles.image}
+          style={{
+            backgroundImage: `url(images/${image})`,
+          }}
+        />
+      </div>
+    </Link>
   );
 }
 
 ProjectCard.propTypes = {
+  projectId: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
